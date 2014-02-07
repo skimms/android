7a76513027d4d2a96e63c8c79060830b5c15f59f
skimms <skimms@naver.com>
2014-02-07 17:04:52 +0900

    엑션바 스타일 테스트

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 //import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -18,9 +19,13 @@ public class MainActivity extends ActionBarActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+                
         setContentView(R.layout.activity_display_message);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        	getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }        
         
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // If your minSdkVersion is 11 or higher, instead use:
         //getActionBar().setDisplayHomeAsUpEnabled(true);
     }
